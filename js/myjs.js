3baea4054bac76016d976b3227e6ed64967c9c8e
carly101 <t1616300@student.southstaffs.ac.uk>
2017-11-29 13:07:47 +0000

    edit

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,4 @@
-$(".button-collapse").sideNav();
+ $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
